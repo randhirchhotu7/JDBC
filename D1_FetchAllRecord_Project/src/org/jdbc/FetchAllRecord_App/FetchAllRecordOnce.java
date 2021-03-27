bf88b4a7b1b1bf887d42cc9196076f2c03dbee2e
Randhir Prakash <61622146+randhirchhotu7@users.noreply.github.com>
2021-03-27 22:33:36 +0530

    Add files via upload

---
+++ D1_FetchAllRecord_Project/src/org/jdbc/FetchAllRecord_App/FetchAllRecordOnce.java
@@ -0,0 +1,71 @@
+//		Code to Fetch multiple Record using getXXX()
+
+package org.jdbc.FetchAllRecord_App;
+import java.sql.*;
+public class FetchAllRecordOnce 
+{
+	public static void main(String[] args) 
+	{
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String qry = "select * from btm.student";
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=thunder");
+			
+			pstmt=con.prepareStatement(qry);
+			
+			rs=pstmt.executeQuery();
+			while(rs.next())
+			{
+				int id=rs.getInt(1);
+//				int id=rs.getInt("id"); // we can also use this varient method//
+				String name=rs.getString(2);
+				double per=rs.getDouble(3);
+				System.out.println("["+id+","+name+","+per+"]");
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(rs!=null)
+			{
+				try
+				{
+					rs.close();
+				} 
+				catch (SQLException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null)
+			{
+				try
+				{
+					pstmt.close();
+				} 
+				catch (SQLException e) 
+				{
+					e.printStackTrace();
+				}
+			}if(con!=null)
+			{
+				try
+				{
+					con.close();
+				} 
+				catch (SQLException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
